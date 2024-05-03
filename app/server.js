2620610d5d4dc0ee16ab5c5c78f7dc64ff6f0312
cyrilletual <cyrille.tual@gmail.com>
2024-05-03 15:50:02 +0200

    5

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Hello World, have a nice day 4eme try !')
+  res.send('Hello World, have a nice day 5eme try !')
 })
 
 app.listen(port, () => {
